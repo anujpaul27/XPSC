3992c84d1fb790e8bf1a4101ce5c2c0dc15766a2
Anuj Paul <125150468+anujpaul27@users.noreply.github.com>
2024-11-03 00:46:16 +0600

    Update p.cpp

--- Day 5/p.cpp
+++ Day 5/p.cpp
@@ -10,6 +10,23 @@ int main ()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        if (a+b == c)
+        {
+            cout<<"+"<<endl;
+        }
+        else 
+        {
+            cout<<"-"<<endl;
+        }
+    }
     
 
     return 0;
