4ce328e827ad24609275898b87ded3ea3731c67a
Anuj Paul <125150468+anujpaul27@users.noreply.github.com>
2024-11-24 01:11:48 +0600

    Sorry-for-late-submit

--- Day 15/C.cpp
+++ Day 15/C.cpp
@@ -1,16 +1,41 @@
-/*
-Solver: Anuj Paul
-link: 
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
-int main ()
+int main()
 {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        string S;
+        cin >> S;
 
+        int zero = 0, one = 0;
+        char prev = S[0];
+
+        if (prev == '0')
+            zero++;
+        else
+            one++;
+
+        for (int i = 1; i < N; i++)
+        {
+            if (S[i] != prev)
+            {
+                if (S[i] == '0')
+                    zero++;
+                else
+                    one++;
+            }
+            prev = S[i];
+        }
+
+        cout << min(zero, one) << "\n";
+    }
     return 0;
 }
