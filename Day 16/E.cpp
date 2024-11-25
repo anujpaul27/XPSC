280efcb425695877faaa59cb7f87c57f699b3e56
Anuj Paul <125150468+anujpaul27@users.noreply.github.com>
2024-11-25 06:37:10 +0600

    Update E.cpp

--- Day 16/E.cpp
+++ Day 16/E.cpp
@@ -11,6 +11,35 @@ int main ()
     cin.tie(nullptr);
 
     
+    int n; long long k;
+    cin>>n>>k;
+
+    vector<int> v (n);
+    for (int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+
+    int l=0, r=0, ans=0;
+    long long sum =0;
+
+    while (r<n)
+    {
+        sum+=v[r];
+
+        if (sum <= k)
+        {
+            ans = max(ans,r-l+1);
+        }
+        else 
+        {
+            sum -= v[l];
+            l++;
+        }
+        r++;
+    }
+
+    cout<<ans<<endl;
 
     return 0;
 }
